f9900caaf76689dda9f6c6901aa5d52b44a18eea
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2017-08-24 18:24:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,18 @@ console.log('Loaded!');
 //img.onclick = function () {
 //    var interval = setInterval(moveRight, 17);
 //};
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
+    
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
